09c51397817c85a849481486acd806178699e857
meenarajesh99 <meenarajesh99@icloud.com>
2026-07-27 16:03:14 -0400

    Updated port configuraton

--- cashtran-server/src/main/java/com/perscholas/cashtran/config/WebSecurityConfig.java
+++ cashtran-server/src/main/java/com/perscholas/cashtran/config/WebSecurityConfig.java
@@ -88,7 +88,7 @@ public class WebSecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
 
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://cashtran-frontend.onrender.com"));
 
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
