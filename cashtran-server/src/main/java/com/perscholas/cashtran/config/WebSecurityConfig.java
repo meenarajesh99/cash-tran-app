65eca4658aa4feffba1ff5ffa937d1c679d11ef3
meenarajesh99 <meenarajesh99@icloud.com>
2026-07-27 16:38:44 -0400

    Added actuator end point to WebSecurityConfig

--- cashtran-server/src/main/java/com/perscholas/cashtran/config/WebSecurityConfig.java
+++ cashtran-server/src/main/java/com/perscholas/cashtran/config/WebSecurityConfig.java
@@ -64,6 +64,7 @@ public class WebSecurityConfig {
                     .requestMatchers(
                         "/api/auth/login",
                         "/api/auth/register",
+                        "/actuator/health",
 
                         // Swagger
                         "/swagger-ui/**",
@@ -88,7 +89,11 @@ public class WebSecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
 
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://cashtran-frontend.onrender.com"));
+    configuration.setAllowedOrigins(
+        List.of(
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "https://cashtran-frontend.onrender.com"));
 
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
